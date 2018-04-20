9d982bd9e409df77498746aa0525234a2c8d060a
lae <laelaeaung007@gmail.com>
2018-04-20 14:48:03 +0900

    add yojson

--- install.sh
+++ install.sh
@@ -1,12 +1,17 @@
 #! /bin/sh
 
-sudo apk add libsodium-dev libffi-dev
-sudo apk add m4 perl
+sudo apk update
+sudo apk add libsodium-dev libffi-dev gcc
+sudo apk add m4 perl 
+sudo pacman -Sy clingo
 
-opam init
+opam install core
+opam init -a
 eval `opam config env`
 opam install -y depext
 opam depext -y conf-gmp.1
+
+opam install -y yojson
 
 opam pin add -n sodium https://github.com/tsh2/ocaml-sodium.git#with_auth_hmac256
 opam pin add -n opium https://github.com/tsh2/opium.git#fix-ssl-option
