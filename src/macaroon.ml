bf5a0079c811e1ceb9ae4af492e9f636eecbf092
lae <laelaeaung007@gmail.com>
2018-04-27 11:32:25 +0900

    changed json object name

--- src/macaroon.ml
+++ src/macaroon.ml
@@ -11,7 +11,7 @@ let s = ref None
         
 (*LLA added*)
   
-let reqJson="{\"deviceId\" :\"*1\",\"dataId\":\"*2\",\"content\":*3}"
+let reqJson="{\"driver_id\" :\"*1\",\"data_id\":\"*2\",\"content\":*3}"
 
 
 (*LLA added end *)
